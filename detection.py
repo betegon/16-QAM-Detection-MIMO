0db6fdaaddbabdc9b1bc4a0a7a81efa21ddbdd19
betegon <miguelbetegongarcia@gmail.com>
2019-11-04 19:16:25 +0100

    From bin to dec

--- detection.py
+++ detection.py
@@ -1,5 +1,7 @@
 import logging
 import numpy as np  
+import streamlit as st
+st.title('MIMO Detection')
 
 #### #### #### [START logger handler] #### #### #### 
 # create logger
@@ -18,17 +20,32 @@ logger.addHandler(ch)
 
 
 # Parameter definition
-M = 16          # Size of signal constellation
-k = np.log2(M)  # Number of bits per symbol
-n = 3000        # Numer of bits to proceess
+M = 16              # Size of signal constellation
+k = int(np.log2(M)) # Number of bits per symbol
+nbits = 3000        # Numer of bits to proceess
 numSamplesPerSymbol = 1    # Oversampling factor
-logger.info("Parameter definition: \n   M = {}\n   k = {}\n   n = {}\n   numSamplesPerSymbol= {}\n".format(M, k, n, numSamplesPerSymbol))
+EbNo = 10           # arbitrarily set to 10 dB
+logger.info("Parameter definition: \n   M = {}\n   k = {}\n   n = {}\n   numSamplesPerSymbol= {}\n"
+            .format(M, k, nbits, numSamplesPerSymbol))
 
 # Create binary data stream
-binaryDataStream = np.random.randint(2,n)
+binaryDataStream = np.random.randint(2, size=(int(nbits/k),k))
 logger.debug("binary DataStream:\n{}".format(binaryDataStream))
 
-# Symbols transmitted
+st.subheader('Binary data Stream')
+st.write(binaryDataStream)
+decDataStream = binaryDataStream.dot(1 << np.arange(binaryDataStream.shape[-1] -1, -1,-1))
+
+st.subheader('# integer symbols transmitted')
+hist_values = np.histogram(decDataStream, bins=M, range=(0,M))[0]
+st.bar_chart(hist_values)   
+
+
+st.subheader('Adding White Gaussian Noise')
+
+
+
+
 x = np.array([[1, 1]]).T
 logger.debug("Symbols transsmited:\n{}".format(x))
 
@@ -43,4 +60,3 @@ logger.debug("Noise samples:\n{}".format(n))
 # Symbols received
 y = h.dot(x) + n
 logger.debug("Symbols received:\n{}".format(y))
-

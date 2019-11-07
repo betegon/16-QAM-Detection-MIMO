d492b349bf6e2a7b682e75fe1b5006ebc4c24f27
betegon <miguelbetegongarcia@gmail.com>
2019-11-07 16:29:11 +0100

    Create Modulation class and scatter plot the constellation

--- detection.py
+++ detection.py
@@ -24,7 +24,7 @@ logger.addHandler(ch)
 # Parameter definition
 M = 16              # Size of signal constellation
 k = int(np.log2(M)) # Number of bits per symbol
-nbits = 3000        # Numer of bits to proceess
+nbits = 3000        # Numer of bits to process
 numSamplesPerSymbol = 1    # Oversampling factor
 logger.info("Parameter definition: \n   M = {}\n   k = {}\n   n = {}\n   numSamplesPerSymbol= {}\n"
             .format(M, k, nbits, numSamplesPerSymbol))
@@ -32,7 +32,7 @@ logger.info("Parameter definition: \n   M = {}\n   k = {}\n   n = {}\n   numSamp
 # Set seed for debugging
 if st.checkbox('Set seed for debugging.'): np.random.seed(1)
 
-
+#### #### #### [START symbols transmitted, x] #### #### #### 
 # Create binary data stream
 binaryDataStream = np.random.randint(2, size=(int(nbits/k),k))
 logger.debug("binary DataStream:\n{}".format(binaryDataStream))
@@ -41,16 +41,32 @@ st.subheader('Binary data Stream')
 st.write(binaryDataStream)
 decDataStream = binaryDataStream.dot(1 << np.arange(binaryDataStream.shape[-1] -1, -1,-1))
 
+
+# PLot decimal Symbols transmitted (x)
 st.subheader('# integer symbols transmitted')
 hist_values = np.histogram(decDataStream, bins=M, range=(0,M))[0]
 st.bar_chart(hist_values)   
+#### #### #### [END symbols transmitted, x] #### #### #### 
 
 
+#### #### #### [START AWGN] #### #### #### 
 st.subheader('Adding White Gaussian Noise')
 
+# Set EbNo
 EbNo = st.slider('Ratio of bit energy to noise power spectral density, Eb/N0:',0,30,10,1)           # arbitrarily set to 10 dB
-st.write(EbNo)
+# Calculate signal-to-noise ratio
+snr = EbNo + 10*np.log10(k) - 10*np.log10(numSamplesPerSymbol)
+st.write("Signal-to-noise ratio achieved: ",snr)
+#### #### #### [END AWGN] #### #### #### 
 
+
+
+
+
+
+
+
+#### #### #### [START MIMO 2x2 System] #### #### #### 
 x = np.array([[1, 1]]).T
 logger.debug("Symbols transsmited:\n{}".format(x))
 
@@ -65,3 +81,4 @@ logger.debug("Noise samples:\n{}".format(n))
 # Symbols received
 y = h.dot(x) + n
 logger.debug("Symbols received:\n{}".format(y))
+#### #### #### [END MIMO 2x2 System] #### #### #### 
---
+++ modulation.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import pandas as pd
+import numpy as np 
+import math
+import matplotlib.pyplot as plt
+
+class Modulation:
+    def __init__(self,M):
+        self.M = M # Size of signal constellation
+        self.k = int(np.log2(M)) # Number of bits per symbol 
+        # nbits = 3000        # Numer of bits to process
+        # numSamplesPerSymbol = 1    # Oversampling factor
+    def plot_constellation(self):
+
+        data = np.array([[-3,-3],[-3, -1], [-3, 1], [-3, 3],
+                        [-1,-3],[-1, -1], [-1, 1], [-1, 3],
+                        [1,-3],[1, -1], [1, 1], [1, 3],
+                        [3,-3],[3, -1], [3, 1], [3, 3], ])
+
+        # Create data
+        N = len(data)
+        color_base = (0,1,0)
+        colors = np.random.rand(N)
+        color_points = (0,1,0)
+        area = np.pi*3
+
+        axes = plt.gca()
+        axes.set_xlim([-10, 10])
+        axes.set_ylim([-10, 10])
+
+        # Plot
+        plt.scatter(data[:,0],data[:,1], s=area, c=color_base, alpha=0.5)
+
+        plt.title('QAM - Constellation')
+        plt.xlabel('x')
+        plt.ylabel('y')
+        st.pyplot()
+        # without using streamlit:
+        # plt.show()
+
+    
+if __name__ == "__main__":
+    qam = Modulation(16)
+    qam.plot_constellation()

a12a5c1f1e3b1e1b29e2893a44de93f6d8a71132
betegon <miguelbetegongarcia@gmail.com>
2019-11-09 18:59:21 +0100

    Add signal input

--- modulation.py
+++ modulation.py
@@ -6,22 +6,23 @@ import pandas as pd
 import numpy as np 
 import math
 import matplotlib.pyplot as plt
+from utils import awgn
+
 
 class Modulation:
+
+
     def __init__(self,M):
         self.M = M # Size of signal constellation
         self.k = int(np.log2(M)) # Number of bits per symbol 
         # nbits = 3000        # Numer of bits to process
         # numSamplesPerSymbol = 1    # Oversampling factor
-    def plot_constellation(self):
+
+
+    def plot_constellation(self,signal=None):
 
         base_constellation_points = np.array([[-3,-3],[-3, -1], [-3, 1], [-3, 3],
                         [-1,-3],[-1, -1], [-1, 1], [-1, 3],
-                        [1,-3],[1, -1], [1, 1], [1, 3],
-                        [3,-3],[3, -1], [3, 1], [3, 3], ])
-
-        points = np.array([[-10,-10],[-10, -10], [-3, 1], [-3, 3],
-                        [-1,-3],[-1, -1], [-0.5, 0.5], [0, 0],
                         [1,-3],[1, -1], [1, 1], [1, 3],
                         [3,-3],[3, -1], [3, 1], [3, 3], ])
 
@@ -30,13 +31,8 @@ class Modulation:
         area = np.pi*7
         color_base = (0,1,0)
         # colors = np.random.rand(N)
-        # color_points = (0,1,0)
+        color_points = (0,1,0)
         
-        # set the limits of the constellation axes
-        axes = plt.gca() # gca = get current axes
-        axes_limit = self.k + round(self.k*0.2)
-        axes.set_xlim([- axes_limit, axes_limit])
-        axes.set_ylim([- axes_limit, axes_limit])
 
         # To add other points than the base ones, just copy next line and change the input.
         plt.scatter(base_constellation_points[:,0],
@@ -45,16 +41,47 @@ class Modulation:
                     c='black',
                     alpha=0.5,
                     marker='*',
-                    label= 'baseline points')
-        axes.legend()
+                    label= 'Baseline')
+      
+        if signal.any():
+            plt.scatter(signal[:,0],
+                    signal[:,1],
+                    s=area, 
+                    c=color_points,
+                    alpha=0.5,
+                    label= 'Signal')
+        
+        # set the limits of the constellation axes
+        axes = plt.gca() # gca = get current axes
+        axes_limit = self.k + round(self.k*0.2)
+        axes.set_xlim([- axes_limit, axes_limit])
+        axes.set_ylim([- axes_limit, axes_limit])
+
         plt.title('{}-QAM  Constellation'.format(self.M))
         plt.xlabel('x')
         plt.ylabel('y')
+        axes.legend()
+
         st.pyplot()
         # without using streamlit:
         # plt.show()
 
-    
 if __name__ == "__main__":
-    qam = Modulation(16)
-    qam.plot_constellation()
+
+    M = 16 
+    snr = 16
+    qam = Modulation(M)
+
+
+    base_constellation_points = np.array([[-3,-3],[-3, -1], [-3, 1], [-3, 3],
+                        [-1,-3],[-1, -1], [-1, 1], [-1, 3],
+                        [1,-3],[1, -1], [1, 1], [1, 3],
+                        [3,-3],[3, -1], [3, 1], [3, 3], ])
+
+    noise_x = awgn(base_constellation_points[:,0].tolist(),snr)
+    noise_y = awgn(base_constellation_points[:,1].tolist(),snr)
+    noise = np.column_stack((noise_x,noise_y))
+    signal = base_constellation_points + noise
+    print(signal)
+
+    qam.plot_constellation(signal)

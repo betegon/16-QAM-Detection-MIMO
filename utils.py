7297ec12e9acd22ceb11bee5c83fde0ee6b1a718
betegon <miguelbetegongarcia@gmail.com>
2019-12-08 12:19:16 +0100

    add np.array support

--- utils.py
+++ utils.py
@@ -9,23 +9,21 @@ def awgn(signal,snr, seed=False):
     """ Generate Aditive White Gaussian Noise (AWGN) from an input signal.
 
     Args:
-        signal (list(float)): Input signal. e.g. signal = [1, 2.3, -1]
-        snr          (float): Signal-to-noise ratio in dB.
-        seed          (bool): Set seed to reproduce results (good for debugging).
-                              Defaults to False.
+        signal  (np.array): Input signal. e.g. signal = np.array([[1, 2.3, -1]])
+        snr        (float): Signal-to-noise ratio in dB.
+        seed        (bool): Set seed to reproduce results (good for debugging).
+                            Defaults to False.
 
     Returns:
-        noise (list(float)): AWGN generated from an input signal.
+        noise (np.array): AWGN generated from an input signal.
 
     """
     if seed: np.random.seed(1)
     sigpower = sum([ math.pow(abs(signal[i]),2) for i in range(len(signal)) ])
     sigpower = sigpower / len(signal)
     noisepower = sigpower / (math.pow(10,snr/10))
-    # noise = math.sqrt(noisepower)*(np.random.uniform(-1,1,size=len(signal)))
     noise = np.random.normal(0, np.sqrt(noisepower), len(signal))
-    return noise
-
+    return np.array([noise]).T
 
 def mapping (decimal_stream):
 

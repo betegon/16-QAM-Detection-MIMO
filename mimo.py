2c904a71c369088c4679b2b240812f53b5684a06
betegon <miguelbetegongarcia@gmail.com>
2019-12-22 10:16:44 +0100

    Move optim function to optim module

--- mimo.py
+++ mimo.py
@@ -131,40 +131,6 @@ print("\n",A.shape)
 # A = [H'*H zeros(2*N,2*N) -H'*y; zeros(2*N,4*N+1); -y'*H zeros(1,2*N) y'*y];
 
 
-def min_tr_WA(k,A):
-    """ Minizime trace(W*A), equations stated in paper "Semidefinite Relaxation
-        for Detection of 16-QAM Signaling in MIMO Channels".
-
-    Args:
-        k  (int): Number of antennas.
-        A  (np.ndarray): Matrix needed to formulate problem to minimize.
-
-    Returns:
-        W (np.ndarray): Optimal W matrix.
-
-    """
-    # Problem data.
-    np.random.seed(1)
-    #k = 2 # number of antennas (TX y RX)
-    zero_array = np.zeros(2*k)
-
-    # Construct the problem.
-    W = cp.Variable((4*k+1,4*k+1), PSD=True)
-
-    objective = cp.Minimize(cp.trace(cp.matmul(W,A)))
-    constraints = [W >> 0,
-               cp.diag(W[0:2*k, 0:2*k]) - W[2*k:4*k,4*k] == 0,
-               cp.diag(W[2*k:4*k, 2*k:4*k]) - 10*W[2*k:4*k,4*k] + 9*np.ones((2*k)) == 0,
-               W[4*k, 4*k] == 1 ]
-
-    # formulate and solve problem
-    prob = cp.Problem(objective, constraints)
-    prob.solve()
-
-    # print("Optimal value", prob.solve())
-    # print("Optimal value of W: ")
-    # print(W.value) # A numpy ndarray.
-    return W.value
 
 W = min_tr_WA(k,A)
 W11=W[0:2*k,0:2*k]

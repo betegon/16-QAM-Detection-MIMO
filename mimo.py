acbdcee1f08340ca649f72b4ae2c54cdffad5c0c
betegon <miguelbetegongarcia@gmail.com>
2019-12-22 12:36:57 +0100

    move nearesPD to utils

--- mimo.py
+++ mimo.py
@@ -1,11 +1,10 @@
-# TODO: 7. Docstring quantiz()
 # TODO: 3. Pensar que meter y que no en el bucle
 # TODO: 4. Pensar como hacer los bucles (invertidos para poder continuar training cuando queramos)
 # TODO: 5. Pensar como ir guardando los resultados en un fichero
 # TODO: 6. Mover las funciones a utils o a donde corresponda.
+# TODO: 7. Refactor loops for maps and zips.(idiomatic pythonic code ;) 
 
-
-from utils import awgn, mapping, gen_symbols, quantiz
+from utils import awgn, gen_symbols, mapping, nearestPD, quantiz
 from optimization import min_tr_WA
 # %%
 import math
@@ -139,8 +138,6 @@ print("W_ED\n",W_ED)
 print("SIMPLE QUANTIZATION\n\n")
 #simple quantization
 valores=W[0:2*k,4*k]
-print("valores",valores.shape)
-print("sssssss",s.shape)
 simple_quantiz = quantiz(valores,s)
 print("quantiz simple niño",simple_quantiz.shape)
 
@@ -163,71 +160,7 @@ simb=[-3,-1, 1 ,3]
 eigen_descomposition = quantiz(eigen_trans,simb)
 
 
-# %%
-from numpy import linalg as la
 
-def nearestPD(A):
-    """Find the nearest positive-definite matrix to input
-    A Python/Numpy port of John D'Errico's `nearestSPD` MATLAB code [1], which
-    credits [2].
-    [1] https://www.mathworks.com/matlabcentral/fileexchange/42885-nearestspd
-    [2] N.J. Higham, "Computing a nearest symmetric positive semidefinite
-    matrix" (1988): https://doi.org/10.1016/0024-3795(88)90223-6
-    """
-
-    B = (A + A.T) / 2
-    _, s, V = la.svd(B)
-
-    H = np.dot(V.T, np.dot(np.diag(s), V))
-
-    A2 = (B + H) / 2
-
-    A3 = (A2 + A2.T) / 2
-
-    if isPD(A3):
-        return A3
-
-    spacing = np.spacing(la.norm(A))
-    # The above is different from [1]. It appears that MATLAB's `chol` Cholesky
-    # decomposition will accept matrixes with exactly 0-eigenvalue, whereas
-    # Numpy's will not. So where [1] uses `eps(mineig)` (where `eps` is Matlab
-    # for `np.spacing`), we use the above definition. CAVEAT: our `spacing`
-    # will be much larger than [1]'s `eps(mineig)`, since `mineig` is usually on
-    # the order of 1e-16, and `eps(1e-16)` is on the order of 1e-34, whereas
-    # `spacing` will, for Gaussian random matrixes of small dimension, be on
-    # othe order of 1e-16. In practice, both ways converge, as the unit test
-    # below suggests.
-    I = np.eye(A.shape[0])
-    k = 1
-    while not isPD(A3):
-        mineig = np.min(np.real(la.eigvals(A3)))
-        A3 += I * (-mineig * k**2 + spacing)
-        k += 1
-
-    return A3
-
-def isPD(B):
-    """Returns true when input is positive-definite, via Cholesky"""
-    try:
-        _ = la.cholesky(B)
-        return True
-    except la.LinAlgError:
-        return False
-
-if __name__ == '__main__':
-    import numpy as np
-    for i in range(10):
-        for j in range(2, 100):
-            A = np.random.randn(j, j)
-            B = nearestPD(A)
-            assert(isPD(B))
-    print('unit test passed!')
-
-# %%
-#randomization
-#from scipy.linalg import cholesky
-#v2=cholesky(W_ED)
-#v=np.linalg.cholesky(W_ED)
 try:
     v= np.linalg.cholesky(W_ED)
     print("Es positiva")

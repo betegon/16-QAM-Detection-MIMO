20a788439088a78500278850c1403063da94136d
betegon <miguelbetegongarcia@gmail.com>
2020-01-07 10:48:57 +0100

    refactoring

--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 # TODO: mapping() to handle multidimensional np.arrays
 
 import math
-import matplotlib as plt
 import numpy as np
 from numpy import linalg as la
 
@@ -10,7 +9,7 @@ def awgn(signal, snr, seed=False):
     """ Generate Aditive White Gaussian Noise (AWGN) from an input signal.
 
     Args:
-        signal  (np.array): Input signal. e.g. signal = np.array([[1, 2.3, -1]])
+        signal  (np.array): Input signal e.g. signal = np.array([[1, 2.3, -1]])
         snr        (float): Signal-to-noise ratio in dB.
         seed        (bool): Set seed to reproduce results (good for debugging).
                             Defaults to False.
@@ -19,15 +18,16 @@ def awgn(signal, snr, seed=False):
         noise (np.array): AWGN generated from an input signal.
 
     """
-    if seed: np.random.seed(1)
-    sigpower = sum([ math.pow(abs(signal[i]),2) for i in range(len(signal)) ])
+    if seed:
+        np.random.seed(1)
+    sigpower = sum([math.pow(abs(signal[i]), 2) for i in range(len(signal))])
     sigpower = sigpower / len(signal)
-    noisepower = sigpower / (math.pow(10,snr/10))
+    noisepower = sigpower / (math.pow(10, snr/10))
     noise = np.random.normal(0, np.sqrt(noisepower), len(signal))
     return np.array([noise]).T
 
 
-def mapping (decimal_stream):
+def mapping(decimal_stream):
     """ Mapping decimal numbers to 16-QAM constellation signal.
 
     Args:
@@ -38,15 +38,15 @@ def mapping (decimal_stream):
 
     """
     map = {
-     "0": [-3,-3],  "1": [-3,-1],  "2": [-3, 3],  "3": [-3, 1],
-     "4": [-1,-3],  "5": [-1,-1],  "6": [-1, 3],  "7": [-1, 1],
-     "8": [ 3,-3],  "9": [ 3,-1], "10": [ 3, 3], "11": [ 3, 1],
-    "12": [ 1,-3], "13": [ 1,-1], "14": [ 1, 3], "15": [ 1, 1]
+     "0":  [-3, -3],  "1": [-3, -1],  "2": [-3, 3],  "3": [-3, 1],
+     "4":  [-1, -3],  "5": [-1, -1],  "6": [-1, 3],  "7": [-1, 1],
+     "8":   [3, -3],  "9":  [3, -1], "10":  [3, 3], "11":  [3, 1],
+     "12":  [1, -3], "13":  [1, -1], "14":  [1, 3], "15":  [1, 1]
     }
 
-    input_signal = np.empty(shape=(len(decimal_stream),2))
+    input_signal = np.empty(shape=(len(decimal_stream), 2))
     for i in range(len(decimal_stream)):
-        for key,value in map.items():
+        for key, value in map.items():
             if decimal_stream[i] == int(key):
                 # print("value:   {}".format(value))
                 # print("signal:  {}".format(input_signal))
@@ -55,7 +55,7 @@ def mapping (decimal_stream):
     return input_signal
 
 
-def gen_symbols(nbits,n):
+def gen_symbols(nbits, n):
     """ Generate n^2-QAM symbols
 
     Args:
@@ -66,9 +66,9 @@ def gen_symbols(nbits,n):
         (np.ndarray): Contains constellation symbols. e.g. [[-1 3],[3, 1]...]
 
     """
-    binaryDataStream = np.random.randint(2, size=(int(nbits/n),n))
-    decDataStream = binaryDataStream.dot(1 << np.arange(binaryDataStream.shape[-1] -1, -1,-1))
-    return  mapping(decDataStream)
+    binaryDataStream = np.random.randint(2, size=(int(nbits/n), n))
+    decDataStream = binaryDataStream.dot(1 << np.arange(binaryDataStream.shape[-1] - 1, -1, -1))
+    return mapping(decDataStream)
 
 
 def quantiz(entry, symbols):
@@ -81,12 +81,12 @@ def quantiz(entry, symbols):
     Returns:
         result (np.array): Array of quantized values.
     """
-    result = np.empty((len(entry),1))
+    result = np.empty((len(entry), 1))
     for i in range(len(entry)):
         minimum = float("inf")
         for val in symbols:
             if abs(val - entry[i]) < minimum:
-                result[i,0] = val
+                result[i, 0] = val
                 minimum = abs(val - entry[i])
     return result
 
@@ -117,8 +117,8 @@ def nearestPD(A):
     # decomposition will accept matrixes with exactly 0-eigenvalue, whereas
     # Numpy's will not. So where [1] uses `eps(mineig)` (where `eps` is Matlab
     # for `np.spacing`), we use the above definition. CAVEAT: our `spacing`
-    # will be much larger than [1]'s `eps(mineig)`, since `mineig` is usually on
-    # the order of 1e-16, and `eps(1e-16)` is on the order of 1e-34, whereas
+    # will be much larger than [1]'s `eps(mineig)`, since `mineig` is usually
+    # on the order of 1e-16, and `eps(1e-16)` is on the order of 1e-34, whereas
     # `spacing` will, for Gaussian random matrixes of small dimension, be on
     # othe order of 1e-16. In practice, both ways converge, as the unit test
     # below suggests.

ba054b779653982363913dd9acc4ec80c1f331f9
betegon <miguelbetegongarcia@gmail.com>
2020-01-07 10:46:29 +0100

    change opt. solver to MOSEK

--- optimization.py
+++ optimization.py
@@ -2,7 +2,7 @@ import cvxpy as cp
 import numpy as np
 
 
-def min_tr_WA(k,A):
+def min_tr_WA(k, A):
     """ Minizime trace(W*A), equations stated in paper "Semidefinite Relaxation
         for Detection of 16-QAM Signaling in MIMO Channels".
 
@@ -15,21 +15,22 @@ def min_tr_WA(k,A):
 
     """
     # Problem data.
-    np.random.seed(1)
-    zero_array = np.zeros(2*k)
+    # np.random.seed(1)
 
     # Construct the problem.
-    W = cp.Variable((4*k+1,4*k+1), PSD=True)
-    objective = cp.Minimize(cp.trace(cp.matmul(W,A)))
+    W = cp.Variable((4*k+1, 4*k+1), PSD=True)
+    objective = cp.Minimize(cp.trace(cp.matmul(W, A)))
     constraints = [
         W >> 0,
-        cp.diag(W[0:2*k, 0:2*k]) - W[2*k:4*k,4*k] == 0,
-        cp.diag(W[2*k:4*k, 2*k:4*k]) - 10*W[2*k:4*k,4*k] + 9*np.ones((2*k)) == 0,
+        cp.diag(W[0:2*k, 0:2*k]) - W[2*k:4*k, 4*k] == 0,
+        cp.diag(
+            W[2*k:4*k, 2*k:4*k]) - 10*W[2*k:4*k, 4*k] + 9*np.ones((2*k)) == 0,
         W[4*k, 4*k] == 1]
 
     # formulate and solve problem
     prob = cp.Problem(objective, constraints)
-    prob.solve()
+    prob.solve(solver=cp.MOSEK, verbose=True, parallel=True)
+    # prob.solve(solver=cp.SCS, verbose=True, parallel=True, max_iters=50000)
 
     # print(W.value) # A numpy ndarray.
     return W.value

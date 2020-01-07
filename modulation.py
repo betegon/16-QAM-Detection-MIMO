2c63ed8028a3a64ee414059406eb2ef5054f93c8
betegon <miguelbetegongarcia@gmail.com>
2020-01-07 11:28:45 +0100

    Remove modulation

--- modulation.py
+++ modulation.py
@@ -1,5 +1,5 @@
 #TODO: Docstrings
-#TODO: Different colors for each symbol 
+#TODO: Different colors for each symbol
 
 from utils import awgn
 import matplotlib.pyplot as plt
@@ -9,10 +9,9 @@ import streamlit as st
 
 class Modulation:
 
-
-    def __init__(self,M):
+    def __init__(self, M):
         self.M = M # Size of signal constellation
-        self.k = int(np.log2(M)) # Number of bits per symbol 
+        self.k = int(np.log2(M)) # Number of bits per symbol
         # nbits = 3000        # Numer of bits to process
         # numSamplesPerSymbol = 1    # Oversampling factor
 
@@ -27,44 +26,30 @@ class Modulation:
         # Create data
         baseline_area = np.pi*9
         baseline_color = 'black'
-        
+
         # if signal argument is given, plot it before baseline signal,
         # So baseline points are over the signal ones, as points of
         # reference.
         if signal.any():
             signal_area = np.pi*1
             signal_color = (0,1,0)
-    
-            '''
-            for x,y in base_constellation_points:
-                if x == -3 and y == -3:
-                    plt.scatter(x,
-                    y,
-                    s=baseline_area, 
-                    c=(1,0,0),
-                    alpha=0.5,
-                    marker='*',
-                    label= 'Baseline')  
-            '''
 
             plt.scatter(signal[:,0],
                 signal[:,1],
-                s=signal_area, 
+                s=signal_area,
                 c=signal_color,
                 alpha=0.5,
                 label= 'Signal')
-    
-
 
         # To add other points than the base ones, just copy next line and change the input.
         plt.scatter(base_constellation_points[:,0],
             base_constellation_points[:,1],
-            s=baseline_area, 
+            s=baseline_area,
             c=baseline_color,
             alpha=0.5,
             marker='*',
             label= 'Baseline')
-      
+
         # set the limits of the constellation axes
         axes = plt.gca() # gca = get current axes
         axes_limit = self.k + round(self.k*0.2)
